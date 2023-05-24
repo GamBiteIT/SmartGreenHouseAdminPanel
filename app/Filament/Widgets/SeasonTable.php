53e813d55fd31b1c478d247f0a9a767cf1f15ec0
Abderahmane Mahdi Gharzouli <abderahmanemahdigharzouli@Abderahmanes-MacBook-Air.local>
2023-05-24 21:08:01 +0100

    Add Plant Model + Attribut for Season Model

--- app/Filament/Widgets/SeasonTable.php
+++ app/Filament/Widgets/SeasonTable.php
@@ -19,10 +19,16 @@ class SeasonTable extends BaseWidget
     protected function getTableColumns(): array
     {
         return [
-            TextColumn::make('name')->label('Name'),
-            TextColumn::make('plant')->label('Plant'),
-            TextColumn::make('duree')->label('Durée')->suffix("    Jours"),
-            TextColumn::make('4season')->label('Season')
+            TextColumn::make('id')->label("ID"),
+            TextColumn::make('name')->label("Name")->sortable()->searchable(),
+            TextColumn::make('plant.name')->label('Plant Name'),
+            TextColumn::make('start_day')->date(),
+            TextColumn::make('end_day')->date(),
+            TextColumn::make('quantity_planty')->suffix("      KG"),
+            TextColumn::make('expected_productivity')->suffix("      KG"),
+            TextColumn::make('4season')->label("Les quatre saisons"),
+            TextColumn::make('productivity')->label("productivity")->suffix("     KG"),
+            TextColumn::make('created_at')->dateTime()
         ];
     }
 }
