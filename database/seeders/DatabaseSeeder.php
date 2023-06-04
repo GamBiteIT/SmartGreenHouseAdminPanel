b75e79e00729862612a255d7e16c5d91a6c18b2e
Abderahmane Mahdi Gharzouli <abderahmanemahdigharzouli@Abderahmanes-MacBook-Air.local>
2023-06-04 22:40:22 +0100

    Add DB SEED

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Parametre;
+use App\Models\User;
+use App\Models\Plant;
+use App\Models\Season;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +17,39 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name' => 'farmer',
+            'email'=>'f@f.com',
+            'password'=>'$2y$10$2m4TIR6PT.pIVPtiIog4TuNRQnKDm376rDRofZkfqTvyvtYcSuHd.'
+        ]);
+        Plant::create([
+            'name'=>'Tomato',
+            'type'=>'Tomato',
+            'duree_de_plontation'=>10,
+            'productivity'=>100,
+            'duree_floration'=>4,
+            'duree_nouaison'=>2,
+            'duree_debut_recolte'=>2,
+            'duree_fin_recorte'=>2
+        ]);
+        Season::create([
+         'name'=>'Season 1',
+         'plant_id'=>1,
+         'start_day'=>'2023-05-01 00:00:00',
+         'end_day'=>'2023-07-15 00:00:00',
+         'quantity_planty'=>20,
+         'expected_productivity'=>100,
+         '4season'=>'éte',
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Parametre::create([
+        'season_id'=>1,
+        'TemperatureValeur_max'=>29,
+        'TemperatureValeur_min'=>16,
+        'HumidityValeur'=>75,
+        'SoilValeur'=>550,
+        'LightValeur_max'=>20000,
+        'LightValeur_min'=>10000
+        ]);
     }
 }
