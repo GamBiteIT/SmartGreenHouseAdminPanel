88a9af37879ea4d43463fadb373e85ab7f82c3ed
Abderahmane Mahdi Gharzouli <abderahmanemahdigharzouli@Abderahmanes-MacBook-Air.local>
2023-06-05 01:12:12 +0100

    Fix Fix FIX XIF GMLKLK

--- app/Filament/Resources/SeasonResource/RelationManagers/ObservationRelationManager.php
+++ app/Filament/Resources/SeasonResource/RelationManagers/ObservationRelationManager.php
@@ -44,14 +44,14 @@ class ObservationRelationManager extends RelationManager
                 //
             ])
             ->headerActions([
-                Tables\Actions\CreateAction::make(),
+
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
-                Tables\Actions\DeleteAction::make(),
+
             ])
             ->bulkActions([
-                Tables\Actions\DeleteBulkAction::make(),
+
             ]);
     }
 }
